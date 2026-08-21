074504981b33cbcff0c5fbb56d6cd8d77eacde21
dhvanit <darjidhvanit@gmail.com>
2026-08-21 17:51:47 +0530

    changes role

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -275,7 +275,7 @@ const Projects = () => {
         const isManagerOrAdmin = (userData) => {
             const role = String(userData?.role || "").trim().toLowerCase();
 
-            return ["manager", "admin"].includes(role);
+            return ["Manager", "Admin"].includes(role);
         };
 
         console.log("Logged In User:", userData);
